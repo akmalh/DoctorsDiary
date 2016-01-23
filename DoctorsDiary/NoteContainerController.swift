0bdde66f34ea5a4fbe35fd90e8549f1be1afde74
akmalh <shuvo707@hotmail.com>
2016-01-24 00:13:41 +1100

    Configured reload table and update table. Optional issue still exists

--- DoctorsDiary/NoteContainerController.swift
+++ DoctorsDiary/NoteContainerController.swift
@@ -11,18 +11,24 @@ import UIKit
 class NoteContainerController: UIViewController,UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var noteList: UITableView!
-    var noteListItems: [String] = ["Apple", "Banana", "Orange"]
-    var noteDateListItems: [String] = ["10/01/2016", "11/02/2016", "23/04/2016"]
+    //var noteListItems: [String] = ["Apple", "Banana", "Orange"]
+    static var noteListItems: [String] = [""]
+    //var noteDateListItems: [String] = ["10/01/2016", "11/02/2016", "23/04/2016"]
+    static var noteDateListItems: [String] = [""]
     
-    var databasePath = NSString()
+    //var databasePath = NSString()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
+        //initializeNotesList()
+        
         noteList.delegate = self
         noteList.dataSource = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,7 +40,7 @@ class NoteContainerController: UIViewController,UITableViewDataSource, UITableVi
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         var rowCount = 0
-        rowCount = noteListItems.count
+        rowCount = NoteContainerController.noteListItems.count
         return rowCount
     }
     
@@ -42,14 +48,14 @@ class NoteContainerController: UIViewController,UITableViewDataSource, UITableVi
     {
         let myCell: UITableViewCell = noteList.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
         
-        myCell.textLabel?.text = noteListItems[indexPath.row]
-        myCell.detailTextLabel?.text = noteDateListItems[indexPath.row]
+        myCell.textLabel?.text = NoteContainerController.noteListItems[indexPath.row]
+        myCell.detailTextLabel?.text = NoteContainerController.noteDateListItems[indexPath.row]
         return myCell
     }
     
     func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath) {
         
-        var itemSelected = noteListItems[indexPath.row]
+        var itemSelected = NoteContainerController.noteListItems[indexPath.row]
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
@@ -62,53 +68,8 @@ class NoteContainerController: UIViewController,UITableViewDataSource, UITableVi
         tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = UITableViewCellAccessoryType.None
     }
 
-    func initializeNotesList ()
-    {
-        let filemgr = NSFileManager.defaultManager()
-        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)
-        
-        let docsDir = dirPaths[0] as! String
-        
-        databasePath = docsDir.stringByAppendingPathComponent("notes.db")
-        
-        let currentDate = NSDate()
-        let formatter = NSDateFormatter()
-        formatter.dateStyle = .MediumStyle
-        //println(formatter.stringFromDate(currentDate))
-        
-        let notesDB = FMDatabase(path: databasePath as String)
-        
-        if notesDB.open() {
-            let querySQL = "SELECT topic, content FROM NOTES WHERE date = '\(formatter.stringFromDate(currentDate))'"
-            
-            let results:FMResultSet? = notesDB.executeQuery(querySQL,
-                withArgumentsInArray: nil)
-            
-            if results?.next() == true {
-//                address.text = results?.stringForColumn("address")
-//                phone.text = results?.stringForColumn("phone")
-//                status.text = "Record Found"
-                println(results?.stringForColumn("topic"))
-                println(results?.stringForColumn("content"))
-            } else {
-//                status.text = "Record not found"
-//                address.text = ""
-//                phone.text = ""
-                
-                println("Record not found")
-            }
-            notesDB.close()
-        } else {
-            println("Error: \(notesDB.lastErrorMessage())")
-        }
-        
-        
-    }
-    
-//    func initializeNotesDatabase ()
+//    func initializeNotesList ()
 //    {
-//        // Check and create DB
-//        
 //        let filemgr = NSFileManager.defaultManager()
 //        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)
 //        
@@ -116,26 +77,40 @@ class NoteContainerController: UIViewController,UITableViewDataSource, UITableVi
 //        
 //        databasePath = docsDir.stringByAppendingPathComponent("notes.db")
 //        
-//        if !filemgr.fileExistsAtPath(databasePath as String) {
+//        let currentDate = NSDate()
+//        let formatter = NSDateFormatter()
+//        formatter.dateStyle = .MediumStyle
+//        //println(formatter.stringFromDate(currentDate))
+//        
+//        let notesDB = FMDatabase(path: databasePath as String)
+//        
+//        if notesDB.open() {
+//            let querySQL = "SELECT topic, content FROM NOTES WHERE date = '\(formatter.stringFromDate(currentDate))'"
 //            
-//            let notesDB = FMDatabase(path: databasePath as String)
+//            let results:FMResultSet? = notesDB.executeQuery(querySQL,
+//                withArgumentsInArray: nil)
 //            
-//            if notesDB == nil {
-//                println("Error: \(notesDB.lastErrorMessage())")
-//            }
-//            
-//            if notesDB.open() {
-//                let sql_stmt = "CREATE TABLE IF NOT EXISTS NOTES (ID INTEGER PRIMARY KEY AUTOINCREMENT, TOPIC TEXT, DATE TEXT, CONTENT TEXT)"
-//                if !notesDB.executeStatements(sql_stmt) {
-//                    println("Error: \(notesDB.lastErrorMessage())")
-//                }
-//                notesDB.close()
+//            if results?.next() == true {
+////                address.text = results?.stringForColumn("address")
+////                phone.text = results?.stringForColumn("phone")
+////                status.text = "Record Found"
+//                
+//                println("From container")
+//                println(results?.stringForColumn("topic"))
+//                println(results?.stringForColumn("content"))
 //            } else {
-//                println("Error: \(notesDB.lastErrorMessage())")
+////                status.text = "Record not found"
+////                address.text = ""
+////                phone.text = ""
+//                
+//                println("Record not found")
 //            }
+//            notesDB.close()
+//        } else {
+//            println("Error: \(notesDB.lastErrorMessage())")
 //        }
 //        
+//        
 //    }
-
 
 }
