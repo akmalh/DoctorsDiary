c70480d2bcb1284f5974838ebb5a15e162bf673e
akmalh <shuvo707@hotmail.com>
2016-01-23 18:08:55 +1100

    Testing data extraction

--- DoctorsDiary/NoteContainerController.swift
+++ DoctorsDiary/NoteContainerController.swift
@@ -13,6 +13,8 @@ class NoteContainerController: UIViewController,UITableViewDataSource, UITableVi
     @IBOutlet weak var noteList: UITableView!
     var noteListItems: [String] = ["Apple", "Banana", "Orange"]
     var noteDateListItems: [String] = ["10/01/2016", "11/02/2016", "23/04/2016"]
+    
+    var databasePath = NSString()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,6 +61,81 @@ class NoteContainerController: UIViewController,UITableViewDataSource, UITableVi
     {
         tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = UITableViewCellAccessoryType.None
     }
+
+    func initializeNotesList ()
+    {
+        let filemgr = NSFileManager.defaultManager()
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)
+        
+        let docsDir = dirPaths[0] as! String
+        
+        databasePath = docsDir.stringByAppendingPathComponent("notes.db")
+        
+        let currentDate = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        //println(formatter.stringFromDate(currentDate))
+        
+        let notesDB = FMDatabase(path: databasePath as String)
+        
+        if notesDB.open() {
+            let querySQL = "SELECT topic, content FROM NOTES WHERE date = '\(formatter.stringFromDate(currentDate))'"
+            
+            let results:FMResultSet? = notesDB.executeQuery(querySQL,
+                withArgumentsInArray: nil)
+            
+            if results?.next() == true {
+//                address.text = results?.stringForColumn("address")
+//                phone.text = results?.stringForColumn("phone")
+//                status.text = "Record Found"
+                println(results?.stringForColumn("topic"))
+                println(results?.stringForColumn("content"))
+            } else {
+//                status.text = "Record not found"
+//                address.text = ""
+//                phone.text = ""
+                
+                println("Record not found")
+            }
+            notesDB.close()
+        } else {
+            println("Error: \(notesDB.lastErrorMessage())")
+        }
+        
+        
+    }
+    
+//    func initializeNotesDatabase ()
+//    {
+//        // Check and create DB
+//        
+//        let filemgr = NSFileManager.defaultManager()
+//        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)
+//        
+//        let docsDir = dirPaths[0] as! String
+//        
+//        databasePath = docsDir.stringByAppendingPathComponent("notes.db")
+//        
+//        if !filemgr.fileExistsAtPath(databasePath as String) {
+//            
+//            let notesDB = FMDatabase(path: databasePath as String)
+//            
+//            if notesDB == nil {
+//                println("Error: \(notesDB.lastErrorMessage())")
+//            }
+//            
+//            if notesDB.open() {
+//                let sql_stmt = "CREATE TABLE IF NOT EXISTS NOTES (ID INTEGER PRIMARY KEY AUTOINCREMENT, TOPIC TEXT, DATE TEXT, CONTENT TEXT)"
+//                if !notesDB.executeStatements(sql_stmt) {
+//                    println("Error: \(notesDB.lastErrorMessage())")
+//                }
+//                notesDB.close()
+//            } else {
+//                println("Error: \(notesDB.lastErrorMessage())")
+//            }
+//        }
+//        
+//    }
 
 
 }
