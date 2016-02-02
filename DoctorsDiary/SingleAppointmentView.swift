ae467907211a9772a62feb140a6bd979c4cb9339
akmalh <shuvo707@hotmail.com>
2016-02-02 20:34:01 +1100

    Testing custom UIView with xib

---
+++ DoctorsDiary/SingleAppointmentView.swift
@@ -0,0 +1,21 @@
+//
+//  SingleAppointmentView.swift
+//  DoctorsDiary
+//
+//  Created by Akmal Hossain on 2/02/2016.
+//  Copyright (c) 2016 Akmal Hossain. All rights reserved.
+//
+
+import UIKit
+
+class SingleAppointmentView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
